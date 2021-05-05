09946bbae98e3b9fbad446d45b424198e110e7f5
rickperez87 <rickperez87@gmail.com>
2021-05-05 15:15:17 -0400

    drop blank rows

--- merge_xl.py
+++ merge_xl.py
@@ -1,5 +1,6 @@
 import tabula
 import pandas as pd
+import numpy as np
 
 # read_pdf will save the pdf table into Pandas Dataframe
 # pdf_df = tabula.read_pdf("DCMP SLY Testing and Inspection Schedule 20210503.pdf", pages='all')[0]
@@ -7,6 +8,12 @@ import pandas as pd
 # pdf_df.columns = pdf_df.iloc[2,:]
 
 # load excel into Pandas Dataframe
+
+#write a function that detects row with first empty row.
+
+
+# return df sliced to first empty row
+
 df1 = pd.read_excel('N99_WHB_SCI_Findings_IT_NCS.xlsx')
 df2 = pd.read_excel('Package B SCI Worksheet_WHB.4.27.21-JS.xlsx')
 df3 = pd.read_excel('Package B SCI Worksheet-SPeterson.xlsx')
@@ -15,14 +22,16 @@ df5 = pd.read_excel('Package B WHB SCI Worksheet_SBGray.xlsx')
 
 frames = [df1,df2,df3,df4,df5]
 
+# print(df2.iloc[100])
+
 def format_df (df_list):
     result = []
     for df in df_list:
-        df = df.iloc[:40,2:9]
+
+        df = df.iloc[:,2:9]
         df.columns = df.iloc[0]
         df = df.iloc[1:,:]
-        # df.dropna(inplace=True)
-        print(df)
+        df.dropna(how='all', subset=["Originator"], inplace=True)
         result.append(df)
     return result
     
