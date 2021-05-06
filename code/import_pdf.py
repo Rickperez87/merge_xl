ccae159652869693322ecebb7f7c993a5b344601
rickperez87 <rickperez87@gmail.com>
2021-05-06 16:49:16 -0400

    remove unwanted row data & commit test data

--- code/import_pdf.py
+++ code/import_pdf.py
@@ -13,31 +13,21 @@ for filename in os.listdir(cwd):
         date=[int(i) for i in filename if i.isdigit()]
         date=f'{date[4]}{date[5]}/{date[6]}{date[7]}/{date[0]}{date[1]}{date[2]}{date[3]}'
         df = tabula.read_pdf(filename,pages='all')[0]
-        df.insert(0,'date','')
-        df['date'] = date
-        frames.append(df)
-
-#function formats df with correct column keys, size, and drops empty rows. returns list of formated frames
-def format_df (df_list):
-    result = []
-    for df in df_list:
         df.columns = df.iloc[2]
+        df.insert(0,'date_name',date)
         df.dropna(how='all', subset=["Company"], inplace=True)
         df.dropna(how='all', axis=1,inplace=True)
-        result.append(df)
-    return result
+        df.drop([0,2], inplace=True)
+        frames.append(df)
 
-#func adds date column with dates based on file name    
+#function formats df with correct column keys, size, and drops empty rows. returns list of formated frames, adds date column with dates based on file name    
 
-new_list = format_df(frames)    
-# print(new_list[0])
+combined_pdf = pd.concat(frames)
 
-result = pd.concat(new_list)
+output_filename = 'testing.xlsx'
 
-excel_output_file = 'testing.xlsx'
-
-# result.to_excel(excel_output_file)
+combined_pdf.to_excel(output_filename)
 
 #open file using bash
 
-# subprocess.Popen([excel_output_file],shell=True)
+subprocess.Popen([output_filename],shell=True)
