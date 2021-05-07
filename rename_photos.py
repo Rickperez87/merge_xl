f0d4d854d27194500028617bd94b83ad5fd8db00
rickperez87 <rickperez87@gmail.com>
2021-05-07 15:41:48 -0400

    remove spacing for better matching

--- rename_photos.py
+++ rename_photos.py
@@ -9,9 +9,11 @@ cwd = os.getcwd()
 
 #got file names from pictures. 
 photos_files=[]
+filename_no_ext_space=[]
 excel=[]
 for filename in os.listdir(cwd):
     if filename.endswith('.jpg' or '.JPG*'):
+        filename_no_ext_space.append(".".join(filename.split('.')[:-1]).strip().replace(' ',''))
         photos_files.append(filename)
     elif filename.endswith('.xlsx'):
         excel.append(filename)
@@ -29,30 +31,22 @@ for file in excel:
     df.drop([0], inplace=True)
     frames.append(df)
 
-# new_list = format_df(frames)    
-
-# result = pd.concat(new_list)
-
-# excel_output_file = 'new.xlsx'
-
-# result.to_excel(excel_output_file)
-
-#open file using bash
-
-# subprocess.Popen([excel_output_file],shell=True)
-
 #create a dictionary with photo key and id values
 df = frames[0]
 
 photos = df['Photos'].values
+photos_no_space=[]
+for photo in photos:
+    photo=str(photo)
+    photos_no_space.append(photo.strip().replace(' ',''))
 id = df['Item Number'].values
-
+# print(photos_no_space)
 id_photo={}
-for i, photo in enumerate(photos, 0):
+for i, photo in enumerate(photos_no_space, 0):
     id_photo[photo]=id[i]
 
-
-for file in photos_files:
-   val = id_photo.get(file)
-   if val: 
-        os.rename(file, f'{val}_{file}' )
+for i, file in enumerate(filename_no_ext_space, 0):
+    val = id_photo.get(file)
+    
+    if val: 
+        os.rename(photos_files[i], f'{val}_{photos_files[i]}' )
